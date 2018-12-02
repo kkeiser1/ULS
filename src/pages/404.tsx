b5add0e457da2068b3b59568241c7d471e63457d
Keiser <kkeiser@emich.edu>
2018-12-02 18:11:20 -0500

    creates 404 & adds to gitignore

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const NotFound: React.SFC = () => {
+  return (
+    <div>
+      <h2>Hmm. An Empty Page. It seems no one has visted here in a while.</h2>
+    </div>
+  );
+};
+export default NotFound;
